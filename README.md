4ddbe1ea4f363b6dd403174d83ce532c233faf5e
santiago0411 <49250130+santiago0411@users.noreply.github.com>
2025-01-02 22:15:06 -0300

    Basic command parsing

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MySQLite
 
 A clone of  [sqlite](https://www.sqlite.org/arch.html) in C.
+
+## Sqlite Architecture
+
+![Showcase](sqlite%20arch.gif)
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include "array.h"
@@ -9,6 +10,15 @@ typedef struct {
     size_t count;
     size_t capacity;
 } StringBuilder;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
 
 void read_input(StringBuilder* sb)
 {
@@ -24,17 +34,69 @@ void read_input(StringBuilder* sb)
     ARRAY_APPEND_MANY(sb, buffer, length);
 }
 
+bool do_meta_command(StringBuilder* sb)
+{
+    if (strcmp(sb->data, ".exit") == 0) {
+        ARRAY_FREE(sb);
+        exit(EXIT_SUCCESS);
+    }
+
+    return false;
+}
+
+bool prepare_statement(StringBuilder* sb, Statement* s)
+{
+    assert(s && "Must provide a valid Statement ptr");
+    if (strncmp(sb->data, "insert", 6) == 0) {
+        s->type = STATEMENT_INSERT;
+        return true;
+    }
+    if (strcmp(sb->data, "select") == 0) {
+        s->type = STATEMENT_SELECT;
+        return true;
+    }
+    return false;
+}
+
+void execute_statement(Statement* s)
+{
+    assert(s && "Must provide a valid Statement ptr");
+    switch (s->type) {
+        case STATEMENT_INSERT:
+            printf("Execute insert...\n");
+            break;
+        case STATEMENT_SELECT:
+            printf("Execute select...\n");
+            break;
+        default:
+            assert(false && "Invalid statement type in execute_statement");
+            break;
+    }
+}
+
 int main(void)
 {
     StringBuilder sb = {0};
     for (;;) {
         printf("db > ");
         read_input(&sb);
-        if (strcmp(sb.data, ".exit") == 0) {
-            ARRAY_FREE(&sb);
-            exit(EXIT_SUCCESS);
+
+        if (sb.data[0] == '.') {
+            if (!do_meta_command(&sb)) {
+                printf("Unknown command '%s'.\n", sb.data);
+            }
+            sb.count = 0;
+            continue;
         }
-        printf("Unknown command '%s'.\n", sb.data);
+
+        Statement statement;
+        if (!prepare_statement(&sb, &statement)) {
+            printf("Unrecognized keyword at the beginning of statement '%s'.\n", sb.data);
+            sb.count = 0;
+            continue;
+        }
+
+        execute_statement(&statement);
         sb.count = 0;
     }
 }
